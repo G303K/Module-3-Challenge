d87889cc6d671d76175cb54c0091ac071cd4efe1
unknown <micder18@gmail.com>
2023-08-21 10:13:36 -0400

    Added changes to html and added functions to script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-// Assignment Code
+// Assignment Code provided
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Function used to generate a random password between 8-128 characters
+function createPassword() {
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var minLength = 8;
+  var maxLength = 128;
+  
+  var passwordLength = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+  
+  var password = "";
+  
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  
+  return password;
+}
+
+// Function to write password to the password variable
 function writePassword() {
-  var password = generatePassword();
+  var password = createPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Event listener added to generate a random passward when the "generate" button is clicked
 generateBtn.addEventListener("click", writePassword);
